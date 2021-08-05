0d9ed03fea54c2d2ad111f31616af875afaa9323
SunLee128 <sunlee128@gmail.com>
2021-08-05 11:16:54 +1000

    Combine fetched words

--- lib/random_words.ex
+++ lib/random_words.ex
@@ -1,17 +1,29 @@
 defmodule RandomWords do
-  @noun "https://random-word-form.herokuapp.com/random/noun"
   @adjective "https://random-word-form.herokuapp.com/random/adjective"
+  @noun "https://random-word-form.herokuapp.com/random/noun"
 
-  def get_nouns do
-    case HTTPoison.get(@adjective) do
+  def start_link, do: GenServer.start_link(__MODULE__, [])
+
+  def combine_words do
+    adj = Task.async(fn -> get_words(@adjective) end)
+    noun = Task.async(fn -> get_words(@noun) end)
+
+    [returned_adj | _] = Task.await(adj)
+    [returned_noun | _] = Task.await(noun)
+
+    "#{returned_adj} #{returned_noun}"
+  end
+
+  def get_words(url) do
+    case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts(body)
+        body |> Jason.decode!()
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts("Not found :(")
+        "Not found" |> Jason.decode!()
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
+        reason |> Jason.decode!()
     end
   end
 end
